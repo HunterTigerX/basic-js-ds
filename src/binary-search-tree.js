1803da31bf0c09a778ffad28974d73122caca123
HunterTigerX <HunterTigerX@gmail.com>
2023-04-04 07:48:57 +0300

    feat: add min max binary tree search

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -47,108 +47,124 @@ class BinarySearchTree {
   has(data) {
     let result = false;
     function checkIfExists(thisClass) {
-        if (thisClass === null) {
-            result = false;
-        } else if (thisClass.data === data) {
-            result = true;
-        } else if (data >= thisClass.data) {
-            checkIfExists(thisClass.right);
-        } else if (data <= thisClass.data) {
-            checkIfExists(thisClass.left);
-        }
+      if (thisClass === null) {
+        result = false;
+      } else if (thisClass.data === data) {
+        result = true;
+      } else if (data >= thisClass.data) {
+        checkIfExists(thisClass.right);
+      } else if (data <= thisClass.data) {
+        checkIfExists(thisClass.left);
+      }
     }
 
     checkIfExists(this.data);
     return result;
-}
+  }
 
   find(data) {
     let result;
     if (this.data === null) {
-        result = null;
+      result = null;
     } else {
-        function checkIfExists(dataX) {
-            if (dataX.data === data) {
-                result = dataX;
-            } else if (dataX.left !== null && data <= dataX.data) {
-                checkIfExists(dataX.left);
-            } else if (dataX.right !== null && data >= dataX.data  ) {
-                checkIfExists(dataX.right);
-            } else {
-                result = null;
-            }
+      function checkIfExists(dataX) {
+        if (dataX.data === data) {
+          result = dataX;
+        } else if (dataX.left !== null && data <= dataX.data) {
+          checkIfExists(dataX.left);
+        } else if (dataX.right !== null && data >= dataX.data) {
+          checkIfExists(dataX.right);
+        } else {
+          result = null;
         }
-        checkIfExists(this.data)
+      }
+      checkIfExists(this.data);
     }
     return result;
-}
+  }
 
-remove(data) {
-  let currentObject = this.data;
-  let route;
-  let i = 0;
-  let checkIfExists = (dataX) => {
-    if (dataX === null) {
-      return null;
-    } else if (dataX.data === data) {
-      if (dataX.right === null && dataX.left === null) {
-        currentObject[route] = null;
-      } else if (dataX.right === null && dataX.left !== null) {
-        currentObject[route] = dataX.left;
-      } else if (dataX.left === null && dataX.right !== null) {
-        currentObject[route] = dataX.right;
-      } else if (dataX.right !== null && dataX.left !== null) {
-        let newDataX;
-        let goDeep2 = (node) => {
-          if (node.left !== null) {
-            //Самый ли глубокий левый узел
-            goDeep2(node.left);
-          } else {
-            let newData = Object.assign({}, node);
-            //Самый глубокий левый узел
-            //Удаляем перемещаемый узел
-            this.remove(newData.data);
-            //Добавляем в переменную данные удалённого узла
-            newDataX = newData.data;
-          }
-        };
-        goDeep2(dataX.right);
-        dataX.data = newDataX;
+  remove(data) {
+    let currentObject = this.data;
+    let route;
+    let i = 0;
+    let checkIfExists = (dataX) => {
+      if (dataX === null) {
+        return null;
+      } else if (dataX.data === data) {
+        if (dataX.right === null && dataX.left === null) {
+          currentObject[route] = null;
+        } else if (dataX.right === null && dataX.left !== null) {
+          currentObject[route] = dataX.left;
+        } else if (dataX.left === null && dataX.right !== null) {
+          currentObject[route] = dataX.right;
+        } else if (dataX.right !== null && dataX.left !== null) {
+          let newDataX;
+          let goDeep2 = (node) => {
+            if (node.left !== null) {
+              //Самый ли глубокий левый узел
+              goDeep2(node.left);
+            } else {
+              let newData = Object.assign({}, node);
+              //Самый глубокий левый узел
+              //Удаляем перемещаемый узел
+              this.remove(newData.data);
+              //Добавляем в переменную данные удалённого узла
+              newDataX = newData.data;
+            }
+          };
+          goDeep2(dataX.right);
+          dataX.data = newDataX;
+        }
+      } else if (data >= dataX.data) {
+        if (i === 0) {
+          i++;
+          route = "right";
+        } else {
+          currentObject = currentObject[route];
+          route = "right";
+        }
+        checkIfExists(dataX.right);
+      } else if (data <= dataX.data) {
+        if (i === 0) {
+          i++;
+          route = "left";
+        } else {
+          currentObject = currentObject[route];
+          route = "left";
+        }
+        checkIfExists(dataX.left);
       }
-    } else if (data >= dataX.data) {
-      if (i === 0) {
-        i++;
-        route = "right";
-      } else {
-        currentObject = currentObject[route];
-        route = "right";
-      }
-      checkIfExists(dataX.right);
-    } else if (data <= dataX.data) {
-      if (i === 0) {
-        i++;
-        route = "left";
-      } else {
-        currentObject = currentObject[route];
-        route = "left";
-      }
-      checkIfExists(dataX.left);
-    }
-  };
+    };
 
-  checkIfExists(this.data);
-}
-
-
+    checkIfExists(this.data);
+  }
 
   min() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    let result;
+    let returnMin = (leftData) => {
+      if (leftData.left === null) {
+        result = leftData.data;
+        return leftData.data;
+      } else {
+        returnMin(leftData.left);
+      }
+    };
+    returnMin(this.data);
+    return result;
   }
 
   max() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    let result;
+    let returnMin = (rightData) => {
+      if (rightData.right === null) {
+        result = rightData.data;
+        return rightData.data;
+      } else {
+        returnMin(rightData.right);
+      }
+    };
+    returnMin(this.data);
+    return result;
   }
 }
 
